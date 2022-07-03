ed312d5044135bf670dce5a01d16e1ff508072c8
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-07-03 13:10:35 +0200

    Don't use filepath for remote paths only for local
    The remote paths are on a Linux system, and need "path".
    
    The local paths can be different, handled by "filepath".
    
    Example:
    
    	Local: "C:\\msys64\\tmp\\lima"
    
    	Remote: "/tmp/lima"
    
    Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

--- pkg/reversesshfs/reversesshfs.go
+++ pkg/reversesshfs/reversesshfs.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -43,7 +44,7 @@ type ReverseSSHFS struct {
 func (rsf *ReverseSSHFS) Prepare() error {
 	sshBinary := rsf.SSHConfig.Binary()
 	sshArgs := rsf.SSHConfig.Args()
-	if !filepath.IsAbs(rsf.RemotePath) {
+	if !path.IsAbs(rsf.RemotePath) {
 		return fmt.Errorf("unexpected relative path: %q", rsf.RemotePath)
 	}
 	if rsf.Port != 0 {
@@ -115,7 +116,7 @@ func (rsf *ReverseSSHFS) Start() error {
 	if !filepath.IsAbs(rsf.LocalPath) {
 		return fmt.Errorf("unexpected relative path: %q", rsf.LocalPath)
 	}
-	if !filepath.IsAbs(rsf.RemotePath) {
+	if !path.IsAbs(rsf.RemotePath) {
 		return fmt.Errorf("unexpected relative path: %q", rsf.RemotePath)
 	}
 	if rsf.Port != 0 {
